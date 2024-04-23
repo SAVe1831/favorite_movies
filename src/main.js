c8094876c9a7deb060b3404aaa23a2f2e2831c86
SAVe1831 <save1831537@gmail.com>
2024-04-23 11:11:46 +0300

    first commit2

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
+import '@/assets/main.css'
 
 createApp(App).use(createPinia()).mount('#app')
